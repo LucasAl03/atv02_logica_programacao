c2afc84d2b1bbfac929a1a20202e1b9128343ad1
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-16 19:51:42 -0300

    Questao 01 completa

---
+++ js/script_questao02.js
@@ -0,0 +1,10 @@
+const btnNum = document.querySelector('#btn-num')
+const Div01 = document.querySelector('#div-01')
+const inputNum = document.querySelector('#num')
+
+let cont = 0
+
+btnNum.addEventListener('click', (evt)=>{
+    
+})
+
