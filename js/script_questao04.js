3d6ee5276cee139a458e2a07a76b8fdc7d38d72a
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-17 12:40:21 -0300

    Questao 04 em desenvolvimento

--- js/script_questao04.js
+++ js/script_questao04.js
@@ -4,17 +4,37 @@ const Div02 = document.querySelector('#div-02')
 const Div03 = document.querySelector('#div-03')
 const Div04 = document.querySelector('#div-04')
 const inputIdade = document.querySelector('#idade')
-const inputSexo = document.querySelector('#sexo')
+const inputM = document.querySelector('#masculino')
+const inputF = document.querySelector('#feminino')
 const inputNome = document.querySelector('#nome')
 
 let cont12 = 0
 let cont18 = 0
 let cont65 = 0
 let cont1865 = 0
+let contM = 0
+let contF = 0
+
+function myFunction(element) {
+    document.getElementById("text1").style.display = "none";
+    document.getElementById("text2").style.display = "none";
+
+    if(element.checked != true) {
+         return true;
+    }
+
+    document.getElementById("masculino").checked = false;
+    document.getElementById("feminino").checked = false;
+
+    document.getElementById(element.id).checked = true;
+    
+}
 
 btnNum.addEventListener('click', (evt)=>{
     let idade = Number(inputIdade.value)
-    let sexo = Number(inputSexo.value)
+    let masculino = Number(inputM.value)
+    let feminino = Number(inputF.value)
+    
 
     if(idade < 18){
         cont18++
@@ -22,24 +42,29 @@ btnNum.addEventListener('click', (evt)=>{
         cont65++
     }else if((idade >= 18) && (idade < 65)){
         cont1865++
-    }
+    }else if(masculino){
+        contM++
+    }else{
+        contF++
+    } 
 
     cont12++
 
-    if(cont12 == 3){
+    if(cont12 == 2){
         inputIdade.setAttribute('disabled', 'disabled')
-        inputPeso.setAttribute('disabled', 'disabled')
         inputNome.setAttribute('disabled', 'disabled')
+        inputM.setAttribute('disabled', 'disabled')
+        inputF.setAttribute('disabled', 'disabled')
 
         Div01.innerHTML = `Total de pessoas com idade inferior a 18 anos: ${cont18}`
         Div02.innerHTML = `Total de pessoas com idade acima de 65 anos: ${cont65}`
-        Div02.innerHTML = `Total de pessoas com idade entre 18 e menor que 65 anos: ${cont1865}`
-        Div02.innerHTML = `Total de pessoas do sexo masculino: ${}. <br> Total de pessoas do sexo feminino ${}.`
+        Div03.innerHTML = `Total de pessoas com idade entre 18 e menor que 65 anos: ${cont1865}`
+        Div04.innerHTML = `Total de pessoas do sexo masculino: ${contM}. <br> Total de pessoas do sexo feminino ${contF}.`
         }
 
     inputIdade.value = ''
-    inputPeso.value = ''
-    inputSexo.value = ''
-
+    inputNome.value = ''
+    inputM.value = ''
+    inputF.value = ''
 })
 
