70a85fa1af97f0f851c121fa4eb5475031548394
Lucas ALberto <lucas.a.santos126@aluno.senai.br>
2026-06-18 20:46:42 -0300

    A

--- js/script_questao04.js
+++ js/script_questao04.js
@@ -1,12 +1,12 @@
 const inputNome = document.querySelector('#name')
 const inputIdade = document.querySelector('#idade')
-const inputM = document.querySelector('#masculino')
-const inputF = document.querySelector('#feminino')
+const inputSexo = document.querySelector('#sexo')
 const btnNum = document.querySelector('#btn-num')
 const divA = document.querySelector('#div-a')
 const divB = document.querySelector('#div-b')
 const divC = document.querySelector('#div-c')
 const divD = document.querySelector('#div-d')
+const ObjPessoas = 
 
 
 cont20 = 0
@@ -16,15 +16,13 @@ cont1865 = 0
 contM = 0
 contF = 0
 
-function onlyOne(checkbox) {
-    var checkboxes = document.getElementsByName(checkbox.name)
-    checkboxes.forEach((item) => {
-        if (item !== checkbox) item.checked = false
-    })
-}
+
 
 btnNum.addEventListener('click', (evt)=>{
     let idade = Number(inputIdade.value)
+    let contF = Text(inputSexo.feminino)
+    let contM = Text(inputSexo.masculino)
+
     
 
     if(idade < 18){
@@ -35,11 +33,30 @@ btnNum.addEventListener('click', (evt)=>{
         cont65++
     }
 
-    if (cont20 == 1) {
+    if(contF, 'checked'){
+        contF
+    }else if(contM, 'checked'){
+        contM
+    }else{
+        alert('Essa área deve ser marcada.')
+    }
+
+    if (cont20 == 2) {
         inputIdade.setAttribute('disabled', 'disabled')
         inputNome.setAttribute('disabled', 'disabled')
+        inputSexo.setAttribute('disabled', 'disabled')
 
-        divA.innerHTML = ``
+        divA.innerHTML = `São ${cont18} as pessoas com idade inferior a 18 anos`
+        divB.innerHTML = `São ${cont65} as pessoas com idade inferior a 18 anos`
+        divC.innerHTML = `São ${cont1865} as pessoas com idade inferior a 18 anos`
+        divD.innerHTML = `São ${contM} as pessoas do sexo masculino. <br>São ${contF} as pessoas do sexo feminino.`
     }
+    
 })
 
+function onlyOne(checkbox) {
+    var checkboxes = document.getElementsByName(checkbox.name)
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+}
