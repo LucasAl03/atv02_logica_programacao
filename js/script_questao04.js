9f99e96e983564172d690617d56372ea49a791cb
Lucas Alberto <lucas.a.santos126@aluno.senai.br>
2026-06-19 20:04:39 -0300

    arquivo index e script questao 4 modificado

--- js/script_questao04.js
+++ js/script_questao04.js
@@ -1,58 +1,11 @@
-const inputNome = document.querySelector('#name')
-const inputIdade = document.querySelector('#idade')
-const inputSexo = document.querySelector('#sexo')
-const btnNum = document.querySelector('#btn-num')
+const pessoas = []
+
+const formPessoa = document.querySelector('#form-pessoa')
+const divLista = document.querySelector('#div-lista')
 const divA = document.querySelector('#div-a')
 const divB = document.querySelector('#div-b')
 const divC = document.querySelector('#div-c')
 const divD = document.querySelector('#div-d')
-const ObjPessoas = 
-
-
-cont20 = 0
-cont18 = 0
-cont65 = 0
-cont1865 = 0
-contM = 0
-contF = 0
-
-
-
-btnNum.addEventListener('click', (evt)=>{
-    let idade = Number(inputIdade.value)
-    let contF = Text(inputSexo.feminino)
-    let contM = Text(inputSexo.masculino)
-
-    
-
-    if(idade < 18){
-        cont18++
-    }else if((idade >= 18) && (idade < 65)){
-        cont1865++
-    }else{
-        cont65++
-    }
-
-    if(contF, 'checked'){
-        contF
-    }else if(contM, 'checked'){
-        contM
-    }else{
-        alert('Essa área deve ser marcada.')
-    }
-
-    if (cont20 == 2) {
-        inputIdade.setAttribute('disabled', 'disabled')
-        inputNome.setAttribute('disabled', 'disabled')
-        inputSexo.setAttribute('disabled', 'disabled')
-
-        divA.innerHTML = `São ${cont18} as pessoas com idade inferior a 18 anos`
-        divB.innerHTML = `São ${cont65} as pessoas com idade inferior a 18 anos`
-        divC.innerHTML = `São ${cont1865} as pessoas com idade inferior a 18 anos`
-        divD.innerHTML = `São ${contM} as pessoas do sexo masculino. <br>São ${contF} as pessoas do sexo feminino.`
-    }
-    
-})
 
 function onlyOne(checkbox) {
     var checkboxes = document.getElementsByName(checkbox.name)
@@ -60,3 +13,39 @@ function onlyOne(checkbox) {
         if (item !== checkbox) item.checked = false
     })
 }
+
+cont20 = 0
+cont18 = 0
+cont65 = 0
+cont1865 = 0
+
+formPessoa.addEventListener('submit', (evt)=>{
+    evt.preventDefault()
+
+    const dadosForm = new FormData(formPessoa)
+
+    const pessoa = {
+        nome: dadosForm.get('nome'),
+        idade: dadosForm.get('idade'),
+        sexo: dadosForm.get('sexo')
+    }
+
+    addPessoa(pessoa)
+
+    formPessoa.reset()
+    
+    
+})
+
+const addPessoa = (objPessoa)=>{
+    pessoas.push(objPessoa)
+
+    listPessoa()
+}
+
+const listPessoa = () =>{
+    pessoas.forEach((elem, i)=>{
+        divLista.innerHTML += `${i + 1} - ${elem.name}, ${elem.idade} anos, do sexo ${elem.sexo}`
+    })
+}
+
